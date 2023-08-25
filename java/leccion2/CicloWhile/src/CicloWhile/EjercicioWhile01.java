3f08a8d42a308367daef14511a15612978d5df7f
Martin Lona <publicidad-dpi@hotmail.com>
2023-08-24 22:27:35 -0300

    Clases de Java con Break, Continue y Labels

--- java/leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -18,6 +18,16 @@ public class EjercicioWhile01 {
         }while(contador < 7);
         
         for( var contando =0; contando < 7; contando++){
+            if (contando % 2 == 0){
+            System.out.println("contando = " + contando);
+            break;
+            }
+        }
+        
+         for( var contando =0; contando < 7; contando++){
+            if (contando % 2 != 0){
+           continue;
+            }
             System.out.println("contando = " + contando);
         }
     }
